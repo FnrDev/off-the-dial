6db0b7f1f84a2d7f04dd5227d3f68031b0d3d7f0
ryousiff <roro2004yousif@gmail.com>
2026-04-09 01:00:35 +0300

    Added puzzle system (clues, code, door)

---
+++ Assets/Scripts/PuzzleManager.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using TMPro;
+
+public class PuzzleManager : MonoBehaviour
+{
+    public static PuzzleManager Instance;
+
+    public string collectedCode = "";
+    public string correctCode = "417";
+    public TextMeshProUGUI inputText;
+    public GameObject door;
+
+    private bool puzzleSolved = false;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void AddDigit(string digit)
+    {
+        if (puzzleSolved) return;
+
+        collectedCode += digit;
+        UpdateInputDisplay(collectedCode);
+        CheckCode();
+    }
+
+    void CheckCode()
+    {
+        if (collectedCode.Length >= correctCode.Length)
+        {
+            if (collectedCode == correctCode)
+            {
+                Debug.Log("✅ CORRECT CODE - Puzzle Solved!");
+                puzzleSolved = true;
+
+                if (inputText != null)
+                {
+                    inputText.text = "Code: " + collectedCode + " ✅";
+                }
+
+                if (door != null)
+                {
+                    door.transform.position += new Vector3(0, 3, 0);
+                }
+            }
+            else
+            {
+                Debug.Log("❌ WRONG CODE - Resetting...");
+
+                if (inputText != null)
+                {
+                    inputText.text = "Wrong Code!";
+                }
+
+                collectedCode = "";
+                Invoke(nameof(ResetInputText), 1.5f);
+            }
+        }
+    }
+void ResetInputText()
+{
+    if (inputText != null && !puzzleSolved)
+    {
+        inputText.text = "Code:";
+    }
+}
+    public void UpdateInputDisplay(string currentInput)
+    {
+        if (inputText != null)
+        {
+            inputText.text = "Code: " + currentInput;
+        }
+    }
+}
