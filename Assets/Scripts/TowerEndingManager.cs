ecae4e19f3a50ce50066d270165c9a67ba882d97
zalmosawi <zalmosawii65@gmail.com>
2026-05-25 21:40:06 +0300

    Good ending sequence - rain, dawn light, ghost dissolves, audio stops

--- Assets/Scripts/TowerEndingManager.cs
+++ Assets/Scripts/TowerEndingManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UHFPS.Runtime;
 
@@ -8,7 +9,9 @@ public class TowerEndingManager : MonoBehaviour
     public float rainFadeTime = 2f;
 
     [Header("Ending")]
-    public bool broadcastComplete = false;
+    public GhostPatrol ghost;
+    public Light directionalLight;
+    public AudioSource ambientHorror;
 
     private bool _rainStarted = false;
 
@@ -30,21 +33,41 @@ public class TowerEndingManager : MonoBehaviour
         var rainModule = GameManager.Module<RainingModule>();
         if (rainModule != null)
             rainModule.FadeRaindrop(true, rainFadeTime);
-
-        Debug.Log("Tower reached - Rain started");
     }
 
     public void TriggerGoodEnding()
     {
-        Debug.Log("GOOD ENDING triggered");
-        StopRain();
-        // TODO: fog lift, dawn light, ghost dissolve
+        StartCoroutine(GoodEndingSequence());
     }
 
-    public void TriggerBadEnding()
+    private IEnumerator GoodEndingSequence()
     {
-        Debug.Log("BAD ENDING triggered");
-        // TODO: fog thickens, ghost climbs
+        // Stop rain
+        StopRain();
+        if (ambientHorror != null)
+            ambientHorror.Stop();
+        AudioListener.volume = 0f;
+        yield return new WaitForSeconds(3f);
+
+        // Warm dawn light
+        if (directionalLight != null)
+        {
+            directionalLight.gameObject.SetActive(true);
+            directionalLight.color =
+                new Color(1f, 0.85f, 0.5f);
+            directionalLight.intensity = 1.5f;
+        }
+        yield return new WaitForSeconds(2f);
+
+        // Dissolve ghost
+        if (ghost != null)
+            ghost.gameObject.SetActive(false);
+
+        yield return new WaitForSeconds(2f);
+
+        // Show ending UI - load main menu or
+        // show credits screen
+        Debug.Log("ENDING COMPLETE");
     }
 
     private void StopRain()
@@ -55,5 +78,33 @@ public class TowerEndingManager : MonoBehaviour
 
         if (rainSplash != null)
             rainSplash.SetActive(false);
+    }
+
+    public void TriggerBadEnding()
+    {
+        StartCoroutine(BadEndingSequence());
+    }
+
+    private IEnumerator BadEndingSequence()
+    {
+        // Make rain heavier
+        var rainModule = GameManager.Module<RainingModule>();
+        if (rainModule != null)
+            rainModule.FadeRaindrop(true, 0.5f);
+
+        yield return new WaitForSeconds(3f);
+
+        // Ghost climbs - move ghost to tower base
+        if (ghost != null)
+        {
+            ghost.transform.position =
+                new Vector3(434.6715f, -53.8532f,
+                -455.6354f);
+        }
+
+        yield return new WaitForSeconds(3f);
+
+        // Game over
+        Debug.Log("ENDING COMPLETE");
     }
 }
